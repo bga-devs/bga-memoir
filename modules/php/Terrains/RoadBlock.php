0bed6892b8a49cae37de8e1f5c453538395d8d9c
vtoper <vincent.toper@gmail.com>
2022-03-27 22:48:20 +0200

    desert roadblock

--- modules/php/Terrains/RoadBlock.php
+++ modules/php/Terrains/RoadBlock.php
@@ -5,7 +5,7 @@ class RoadBlock extends \M44\Models\Terrain
 {
   public static function isTileOfType($hex)
   {
-    return in_array($hex['name'], ['roadblock']);
+    return in_array($hex['name'], ['roadblock', 'droadblock']);
   }
 
   public function __construct($row)
