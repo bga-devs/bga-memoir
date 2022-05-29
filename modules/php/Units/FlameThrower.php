a56e787c0db1f45e94d1d0bfa4e0b75d318f703d
vtoper <vincent.toper@gmail.com>
2022-05-29 23:22:11 +0200

    fix 64740

--- modules/php/Units/FlameThrower.php
+++ modules/php/Units/FlameThrower.php
@@ -5,10 +5,11 @@ class FlameThrower extends Armor
 {
   public function __construct($row)
   {
+    parent::__construct($row);
+
     $this->number = '13';
     $this->name = clienttranslate('Flame Thrower Tank');
     $this->maxMalus = -1;
     $this->desc[] = clienttranslate('Terrain battle dice reduction limited to 1 max in Close Assault');
-    parent::__construct($row);
   }
 }
